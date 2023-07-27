1afdf26c3a5d582276f220d5de66bf0bb576a1fe
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-07-27 16:25:59 +0530

    Employee is Present or Absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,8 @@
 console.log("Welcome to the Employee Wage Computation");
+const IS_ABSENT =0;
+let empcheck=Math.floor(Math.random()*10)%2;
+if ( empcheck ==IS_ABSENT){
+    console.log("Employee is Absent");
+}else{
+    console.log("Employee is Present");
+}
